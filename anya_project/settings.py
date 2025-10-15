4f721b502e2f139997a4a74e3b0972d1bdcd952b
DESKTOP-GAPK4N2\Hank <hank200702@gmail.com>
2025-10-15 18:13:25 +0800

    fix(deploy): 補上 STATIC_ROOT 設定以修復 Heroku 部署錯誤

--- anya_project/settings.py
+++ anya_project/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Django
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
